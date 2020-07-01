e261421451caa91c869343e4415763b7a83518f9
duanyuxiang <duanyx@belink.com>
2020-07-01 16:55:21 +0800

    提交代码

--- main.c
+++ main.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-void main() {
-    int n, a, sum = 0, i = 0;
-    scanf("%d", &n);
-    while (n) {
-        a = n % 8;
-        n = n / 8;
-        sum += a * pow(10, i);
-        i++;
+//MrHaddis
+//https://github.com/MrHaddis/PAT1022
+
+int main() {
+    //数A 数B a+b的和
+    long a, b, sum;
+    //进制
+    int d;
+    //存放余数
+    int sumArray[100];
+    //定义下标
+    int count = 0;
+    //获取输入
+    scanf("%ld %ld %d", &a, &b, &d);
+    //求和
+    sum = a + b;
+    //为0的时候 直接输入0
+    if (sum == 0) {
+        printf("0");
+        return 0;
     }
-    printf("%d", sum);
+    //循环求余数并存储
+    while (sum != 0) {
+        sumArray[count++] = sum % d;
+        //每求一次就除以10
+        sum = sum / d;
+    }
+    //倒序输出结果
+    for (int i = count - 1; i >= 0; i--) {
+        printf("%d", sumArray[i]);
+    }
+    return 0;
 }
